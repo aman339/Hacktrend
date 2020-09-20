d000467c1a0735065caafa52567195973de23ea9
Aman Choudhary <66405145+aman339@users.noreply.github.com>
2020-09-20 12:20:10 +0530

    Updates

---
+++ precache-manifest.a26843cf15c4a48f08e62dbc22e109ab.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910b26b0c4cfd8f2cf4f1ab256a59767",
+    "url": "/Hacktrend/index.html"
+  },
+  {
+    "revision": "8bb1d158871f305df70f",
+    "url": "/Hacktrend/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "23a07acc7b70cc6854d5",
+    "url": "/Hacktrend/static/css/main.6db75c17.chunk.css"
+  },
+  {
+    "revision": "8bb1d158871f305df70f",
+    "url": "/Hacktrend/static/js/2.dbcec709.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Hacktrend/static/js/2.dbcec709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a07acc7b70cc6854d5",
+    "url": "/Hacktrend/static/js/main.150ca7e8.chunk.js"
+  },
+  {
+    "revision": "9eb9d3302117690ac94a",
+    "url": "/Hacktrend/static/js/runtime-main.6d58179f.js"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/Hacktrend/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "3ee1f27f75aaee8e802d9f94b7697c00",
+    "url": "/Hacktrend/static/media/pinto.3ee1f27f.svg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/Hacktrend/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "941b999b2be98baa97685ce850342864",
+    "url": "/Hacktrend/static/media/way13.941b999b.jpg"
+  },
+  {
+    "revision": "ded92f215ac0fbac86180edd376ef832",
+    "url": "/Hacktrend/static/media/way2.ded92f21.jpg"
+  },
+  {
+    "revision": "8c8fc941040bf9faa9fa468688351b6f",
+    "url": "/Hacktrend/static/media/way3.8c8fc941.jpg"
+  },
+  {
+    "revision": "031003d13a27298d35c9f76f2630f6f5",
+    "url": "/Hacktrend/static/media/way4.031003d1.jpg"
+  },
+  {
+    "revision": "4a46db7b8acf97f384bfadc409167c50",
+    "url": "/Hacktrend/static/media/way5.4a46db7b.jpg"
+  },
+  {
+    "revision": "a53c52a6bcabf4f81355a09a76c5489c",
+    "url": "/Hacktrend/static/media/way6.a53c52a6.jpg"
+  },
+  {
+    "revision": "06362f27933158dc02278ee97d5539af",
+    "url": "/Hacktrend/static/media/way9.06362f27.jpg"
+  }
+]);
